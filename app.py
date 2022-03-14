4ddecdf68bbe7a231222c6f52c6c77d4deaaf07e
alice <alice@wonderland.com>
2022-03-14 13:47:49 +0200

    corrected sqlalchemy track modifications

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
-app.config['TRACK_DATABASE_MODIFICATION'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 @app.route('/')
