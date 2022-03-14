0971d122dc6e93f0ddf084dd208d0012c1012741
alice <alice@wonderland.com>
2022-03-14 13:45:44 +0200

    added sqlalchemy

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+app.config['TRACK_DATABASE_MODIFICATION'] = False
 
+db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return render_template('base.html')
